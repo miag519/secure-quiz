a7023038d085e8c57aa3ed63582c7ac5fe4d491c
miag519 <miag1984.5@gmail.com>
2024-05-09 10:19:59 -0700

    still working on score

--- webapp.py
+++ webapp.py
@@ -26,6 +26,80 @@ def startOver():
 def renderPage1():
     return render_template('page1.html')
     
+@app.route('/page2',methods=['GET','POST'])
+def renderPage2():
+    error = None
+    if 'which1' in session:
+        error = "no going back! try again"
+    else:
+        session["which1"]=request.form['which1']
+        return render_template('page2.html')
+    session.clear()
+    return render_template('home.html', error=error)
+
+
+@app.route('/page3',methods=['GET','POST'])
+def renderPage3():
+    error = None
+    if 'which2' in session:
+        error = "no going back! try again"
+    else:
+        session["which2"]=request.form['which2']
+        return render_template('page3.html')
+    session.clear()
+    return render_template('home.html', error=error)
+    
+@app.route('/page4',methods=['GET','POST'])
+def renderPage4():
+    error = None
+    if 'which3' in session:
+        error = "no going back! try again"
+    else:
+        session["which3"]=request.form['which3']
+        return render_template('page4.html')
+    session.clear()
+    return render_template('home.html', error=error)
+    
+@app.route('/page5',methods=['GET','POST'])
+def renderPage5():
+    error = None
+    if 'which4' in session:
+        error = "no going back! try again"
+    else:
+        session["which4"]=request.form['which4']
+        return render_template('page5.html')
+    session.clear()
+    return render_template('home.html', error=error)
+    
+@app.route('/page6',methods=['GET','POST'])
+def renderPage6():
+    error = None
+    if 'which5' in session:
+        error = "no going back! try again"
+    else:
+        session["which5"]=request.form['which5']
+        return render_template('page6.html')
+    session.clear()
+    return render_template('home.html', error=error)
+    
+@app.route('/page7',methods=['GET','POST'])
+def renderPage7():
+    if 'which6' in session:
+        error = "no going back! try again"
+    else:
+        session["which6"]=request.form['which6']
+        results = result()
+        ans1 = answer1()
+        ans2 = answer2()
+        ans3 = answer3()
+        ans4 = answer4()
+        ans5 = answer5()
+        ans6 = answer6()
+        return render_template('page7.html', result = results, answer1 = ans1, answer2 = ans2, answer3 = ans3, 
+        answer4 = ans4, answer5 = ans5, answer6 = ans6)
+        session.clear()
+        return render_template('home.html', error=error)
+    
 def result():
     result = 0
     if session["which1"] == "star":
@@ -42,38 +116,62 @@ def result():
     
     if session["which5"] == "hydrus":
         result += 1
+    
+    if session["which6"] == "three":
+        result += 1
+        
+    return result
+	
+    
+def answer1():
+    answer1 = None
+    if session['which1'] == "star":
+        answer1 = "Correct"
+    else: 
+        answer1 = "Incorrect"
+    return answer1
+	
+def answer2():
+    answer2 = None
+    if session['which2'] == "orion":
+        answer2 = "Correct"
+    else: 
+        answer1 = "Incorrect"
+    return answer1
+	
+def answer3():
+    answer3 = None
+    if session['which3'] == "cygnus":
+        answer3 = "Correct"
+    else: 
+        answer3 = "Incorrect"
+    return answer3
+	
+def answer4():
+    answer4 = None
+    if session['which4'] == "draco":
+        answer4 = "Correct"
+    else: 
+        answer4 = "Incorrect"
+    return answer4
+	
+def answer5():
+    answer5 = None
+    if session['which5'] == "hydrus":
+        answer5 = "Correct"
+    else: 
+        answer5 = "Incorrect"
+    return answer5
+    
+def answer6():
+    answer6 = None
+    if session['which6'] == "three":
+        answer6 = "Correct"
+    else: 
+        answer6 = "Incorrect"
+    return answer6
+    
 
-@app.route('/page2',methods=['GET','POST'])
-def renderPage2():
-    session["which1"]=request.form['which1']
-    return render_template('page2.html')
-
-@app.route('/page3',methods=['GET','POST'])
-def renderPage3():
-    session["which2"]=request.form['which2']
-    return render_template('page3.html')
-    
-@app.route('/page4',methods=['GET','POST'])
-def renderPage4():
-    session["which3"]=request.form['which3']
-    return render_template('page4.html')
-    
-@app.route('/page5',methods=['GET','POST'])
-def renderPage5():
-    session["which4"]=request.form['which4']
-    return render_template('page5.html')
-    
-@app.route('/page6',methods=['GET','POST'])
-def renderPage6():
-    session["which5"]=request.form['which5']
-    return render_template('page6.html')
-    
-@app.route('/page7',methods=['GET','POST'])
-def renderPage7():
-    session["answer"]=request.form['answer']
-    return render_template('page7.html', result=result)
-    
-
 
 
 
@@ -81,8 +179,6 @@ def renderPage7():
     
     
 
-
-    
 
     
 if __name__=="__main__":
